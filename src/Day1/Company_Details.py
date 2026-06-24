6d3aa9cd496c87806ae329c888dae75825ff333a
Abhilash A <abhilash.a@diggibyte.com>
2026-06-24 03:52:29 -0700

    Company_Details will now print company name and designation

--- src/Day1/Company_Details.py
+++ src/Day1/Company_Details.py
@@ -1,1 +1,6 @@
 print(" This project will print Company Details ")
+
+company_name=input("Enter the company Name :")
+designation=input("Enter your designation :")
+print(f"I work at {company_name}")
+print(f"I work as a {designation}")
